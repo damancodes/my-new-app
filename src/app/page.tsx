c2ecf710d01e809a12985a400e0dd468bdf652c9
damanpreet <damanpreet1243@gmail.com>
2024-06-27 23:52:25 +0530

    greeting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <p>Hey {process.env.myname} here </p>
+      <p>{process.env.greeting} </p>
     </>
   );
 }
